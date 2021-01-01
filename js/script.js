edb277e9b07bfbaf6923c2fb5485bb9c464892e1
Sanjay Singh <30376256+SanjaySRocks@users.noreply.github.com>
2021-01-01 13:00:50 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,43 +1,25 @@
 $(document).ready(function() {
 
-$("#gstbtn").click(function(){
-	CalGST();
-});
+	$("#fuelbtn").click(function(){
+		CalFuel();
+	});
 
-$("#fuelbtn").click(function(){
-	CalFuel();
-});
+	function CalFuel()
+	{
+		var a = $("#fuel_distance").val();
+		var b = $("#fuel_average").val();
+		var c = $("#fuel_cost").val();
 
-function CalGST()
-{
-	var a = $("#amount").val();
-	var b = $("#gstpercent").val();
+		a = a || 0;
+		
+		var a1 = parseFloat(a) / parseFloat(b);
+		var t1 = parseFloat(a1) * parseFloat(c);
 
-	a = a || 0;
-	
-	var gst = parseFloat(a) * parseFloat(b) / 100;
-	var total = parseFloat(a) + parseFloat(gst);
+		a1 = parseFloat(a1).toFixed(2);
+		t1 = parseFloat(t1).toFixed(2);
 
-	$("#gstamt").val(gst);
-  	$("#tamt").val(total);
-}
-
-function CalFuel()
-{
-	var a = $("#fuel_distance").val();
-	var b = $("#fuel_average").val();
-	var c = $("#fuel_cost").val();
-
-	a = a || 0;
-	
-	var a1 = parseFloat(a) / parseFloat(b);
-	var t1 = parseFloat(a1) * parseFloat(c);
-
-	a1 = parseFloat(a1).toFixed(2);
-	t1 = parseFloat(t1).toFixed(2);
-
-	$("#fuel_total_litres").val(a1 + " Ltrs");
-  	$("#total_fuel").val(t1 + " Rs.");
-}
+		$("#fuel_total_litres").val(a1 + " Ltrs");
+		$("#total_fuel").val(t1 + " Rs.");
+	}
 
 });
